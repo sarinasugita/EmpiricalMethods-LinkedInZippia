f1002a7ebe4b370612e114e92b99a10baf7e1094
Sarina Sugita <sarinasugitax@gmail.com>
2023-03-26 23:56:33 -0400

    added industry, seniority, and job function

--- jobDescriptionScraper.py
+++ jobDescriptionScraper.py
@@ -10,6 +10,10 @@ driver = webdriver.Chrome('/Applications/chromedriver_mac64/chromedriver')
 
 job_details = pd.read_csv('job_details.csv')
 job_descriptions = []
+seniority_levels = []
+industries = []
+job_functions = []
+
 print(len(job_details))
 exception_count = 1
 exception2_count = 1
@@ -27,27 +31,36 @@ for i in range(len(job_details)):
         description = driver.find_element(By.CLASS_NAME, "description__text").get_attribute("innerText")
     except:
         description = ""
-        print("exception2")
     job_descriptions.append(description)
-"""
-    #click on show more button
-    try:
-        driver.find_element(By.CLASS_NAME, "show-more-less-html__button").click()
-    except:
-        print(str(exception_count) + "exception")
-        exception_count = exception_count + 1
     
     try:
-        description = driver.find_element(By.CLASS_NAME, "description__text").get_attribute("innerText")
+        seniority_level0 = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[1]/span').get_attribute("innerText")
     except:
-        print(str(exception2_count) + "exception2")
-        exception2_count = exception2_count + 1
-        description = ""
+        seniority_level0 = ""
+    seniority_levels.append(seniority_level0)
+    
+    try:
+        industry0 = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[4]/span').get_attribute("innerText")
+    except:
+        industry0 = ""
+    industries.append(industry0)
 
-"""
+    try:
+        job_function0 = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/section[1]/div/ul/li[3]/span').get_attribute("innerText")
+    except:
+        job_function0 = ""
+    job_functions.append(job_function0)
 
 
-df = pd.DataFrame(job_descriptions)
+# Store job details in a dictionary
+job_dict = {"Job Function": job_functions,
+            "Industry": industries,
+            "Seniority": seniority_levels,
+            "Job Description": job_descriptions}
+
+# Create a DataFrame from the job details dictionary
+df = pd.DataFrame(job_dict)
+
 df.to_csv("job_descriptions.csv")
 driver.quit()
 #job_details.insert(4, column="Job Description", value = job_details)
