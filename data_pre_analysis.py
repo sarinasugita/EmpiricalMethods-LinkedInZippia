9d60db553b65cc362e18408262045cc3564a4e37
Abigail Baldauf <baldauf_abigail@Abigails-Laptop.local>
2023-04-17 21:46:31 -0400

    print pre analysis graphs

---
+++ data_pre_analysis.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import *
+import os
+
+path = 'LIWC_Clean_Results/Seniority'
+
+files = os.listdir(path)
+female_means = []
+female_stds = []
+male_means = []
+male_stds = []
+file_names = []
+full_female = []
+full_male = []
+for f in files:
+    if f == 'Government_Cleaned.csv':
+        continue
+    if f == '.DS_Store':
+        continue
+   # f = 'Government_Cleaned.csv'
+    full_name = path + '/' + f
+    print(f)
+    data = read_csv(full_name)
+    female = data['female'].tolist()
+    print(female)
+    female = np.array(female)
+    print(female)
+    print(np.shape(female))
+    print(np.shape(full_female))
+    female = female[~np.isnan(female)]
+    full_female = np.concatenate([full_female, female])
+
+    male = data['male'].tolist()
+    male = np.array(male)
+    male = male[~np.isnan(male)]
+    full_male = np.concatenate([full_male, male])
+    #full_male = full_male + male
+
+    female_mean = np.mean(female)
+    male_mean = np.mean(male)
+
+    female_std = np.std(female)
+    female_std = np.std(female, ddof=1) / np.sqrt(np.size(female))
+    male_std = np.std(male)
+    male_std = np.std(male, ddof=1) / np.sqrt(np.size(male))
+
+    file_names.append(f.split('.')[0])
+    female_means.append(female_mean)
+    male_means.append(male_mean)
+    female_stds.append(female_std)
+    male_stds.append(male_std)
+
+
+#male_means = np.mean(full_male)
+print(len(full_male))
+print(full_male)
+print(len(full_female))
+#female_means = np.mean(full_female)
+#male_stds = np.std(full_male)
+#male_stds = np.std(full_male, ddof=1) / np.sqrt(np.size(full_male))
+#female_stds = np.std(full_female)
+ind = np.arange(len(file_names))  # the x locations for the groups
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind - width/2, male_means, width, yerr=male_stds,
+                label='Male')
+rects2 = ax.bar(ind + width/2, female_means, width, yerr=female_stds,
+                label='Female')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.set_xticks(ind)
+ax.set_xticklabels((file_names))
+ax.legend()
+
+
+def autolabel(rects, xpos='center'):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
+
+    *xpos* indicates which side to place the text w.r.t. the center of
+    the bar. It can be one of the following {'center', 'right', 'left'}.
+    """
+
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0, 'right': 1, 'left': -1}
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(offset[xpos]*3, 3),  # use 3 points offset
+                    textcoords="offset points",  # in both directions
+                    ha=ha[xpos], va='bottom')
+
+
+#autolabel(rects1, "left")
+#autolabel(rects2, "right")
+
+fig.tight_layout()
+
+plt.show()
+
+
+
